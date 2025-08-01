3249982548bc761d4c78a9dfde2d1b73ccc32cc3
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-01 13:33:29 -0700

    fi: missing service

--- files/scripts/enableservices.sh
+++ files/scripts/enableservices.sh
@@ -1,9 +1,4 @@
 systemctl unmask sshd
 systemctl enable sshd
 
-systemctl enable install-key.service
-
 chmod 600 /etc/NetworkManager/system-connections/static.nmconnection
-
-nmcli connection reload
-nmcli connection up static-enp1s0
