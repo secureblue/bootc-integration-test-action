f279b7467e1d8b747ebd619ff2ef56b5a3e63a01
RoyalOughtness <129108030+RoyalOughtness@users.noreply.github.com>
2025-08-01 13:24:38 -0700

    chore: various fixes

--- files/scripts/enableservices.sh
+++ files/scripts/enableservices.sh
@@ -2,3 +2,8 @@ systemctl unmask sshd
 systemctl enable sshd
 
 systemctl enable install-key.service
+
+chmod 600 /etc/NetworkManager/system-connections/static.nmconnection
+
+nmcli connection reload
+nmcli connection up static-enp1s0
