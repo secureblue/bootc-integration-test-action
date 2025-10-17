8cb13e31debc343317d2751faf036d982f922c30
Daniel Hast <hast.daniel@protonmail.com>
2025-10-17 15:07:03 -0400

    fix: unmask sshd-unix-local.socket
    This socket only exists at runtime, so we can't enable it, but we can
    still unmask it.
    
    Signed-off-by: Daniel Hast <hast.daniel@protonmail.com>

--- files/scripts/enableservices.sh
+++ files/scripts/enableservices.sh
@@ -21,6 +21,10 @@ systemctl enable sshd.service
 systemctl unmask sshd.socket
 systemctl enable sshd.socket
 
+# sshd-unix-local.socket only exists at runtime so we can unmask it
+# but cannot enable it at build-time.
+systemctl unmask sshd-unix-local.socket
+
 systemctl unmask sshd-keygen.target
 systemctl enable sshd-keygen.target
 
